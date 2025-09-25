08d5032081bdbcd2eaf93857916b60baced1a2df
Guilherme006 <guilherme_lazari@outlook.com>
2025-09-25 01:24:07 -0300

    feat(perfil): mapeia caracteríosticas e peso por trilha

--- base_conhecimento.pl
+++ base_conhecimento.pl
@@ -4,3 +4,24 @@ trilha(desenvolvimento_web, 'Web: construcao de apps web frontend/backend.').
 trilha(ciencia_de_dados, 'CD: analise e interpretacao de dados.').
 trilha(seguranca_da_informacao, 'SegInfo: defesa, riscos e conformidade.').
 trilha(redes_e_infraestrutura, 'Redes/Infra: redes, sistemas e cloud.').
+
+% Caracteristicas - peso 1 a 5
+perfil(inteligencia_artificial, logica_programacao, 5).
+perfil(inteligencia_artificial, matematica_estatistica, 5).
+perfil(inteligencia_artificial, programacao_python, 4).
+
+perfil(desenvolvimento_web, design_visual, 3).
+perfil(desenvolvimento_web, programacao_javascript, 5).
+perfil(desenvolvimento_web, bancos_de_dados, 4).
+
+perfil(ciencia_de_dados, matematica_estatistica, 5).
+perfil(ciencia_de_dados, programacao_python, 5).
+perfil(ciencia_de_dados, storytelling_dados, 3).
+
+perfil(seguranca_da_informacao, redes, 4).
+perfil(seguranca_da_informacao, sistemas_operacionais, 4).
+perfil(seguranca_da_informacao, conformidade, 3).
+
+perfil(redes_e_infraestrutura, redes, 5).
+perfil(redes_e_infraestrutura, sistemas_operacionais, 4).
+perfil(redes_e_infraestrutura, cloud, 4).
