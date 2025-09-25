03381356816b2e48f19ff1141e11501aeca02390
Guilherme006 <guilherme_lazari@outlook.com>
2025-09-25 01:21:01 -0300

    feat(trilhas): Cria 5 trilhas de especialização

---
+++ base_conhecimento.pl
@@ -0,0 +1,6 @@
+% Trilhas 
+trilha(inteligencia_artificial, 'IA: modelos e resolucao de problemas complexos.').
+trilha(desenvolvimento_web, 'Web: construcao de apps web frontend/backend.').
+trilha(ciencia_de_dados, 'CD: analise e interpretacao de dados.').
+trilha(seguranca_da_informacao, 'SegInfo: defesa, riscos e conformidade.').
+trilha(redes_e_infraestrutura, 'Redes/Infra: redes, sistemas e cloud.').
